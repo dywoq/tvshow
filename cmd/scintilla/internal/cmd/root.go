714f1793c6cec85117f72df31b6a213a480f17a4
dywoq <dywoq.contact@gmail.com>
2026-09-19 16:42:56 +0300

    cmd/scintilla/internal/cmd: Add usage and short description

--- cmd/scintilla/internal/cmd/root.go
+++ cmd/scintilla/internal/cmd/root.go
@@ -3,6 +3,9 @@ package cmd
 import "github.com/spf13/cobra"
 
 func Root() *cobra.Command {
-	r := &cobra.Command{}
+	r := &cobra.Command{
+		Use:   "scintilla",
+		Short: "Compile your Scintilla code into the bytecode",
+	}
 	return r
 }
