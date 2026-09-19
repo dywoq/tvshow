cc37ff95f851bf26e6196ecd6ef65a392ddaa64f
dywoq <dywoq.contact@gmail.com>
2026-09-19 19:16:32 +0300

    game/core: Initialize the program.

--- game/run.go
+++ game/run.go
@@ -1,16 +1,28 @@
 package game
 
-import "tvshow/game/core"
+import (
+	"fmt"
+	"tvshow/game/core"
+)
 
 // Run configures the game's window, its executors and starts the game.
 func Run() error {
+	err := core.InitializeProgram()
+	if err != nil {
+		return fmt.Errorf("initializing program failed: %v", err)
+	}
+
 	w := &core.Window{
 		Width:  640,
 		Height: 480,
 		Title:  "ТВ Шоу 2",
+		Executors: []core.Executor{
+			core.ProgramExecutor,
+		},
 	}
 	if err := w.Run(); err != nil {
-		return err
+		return fmt.Errorf("running the game failed: %v", err)
 	}
+
 	return nil
 }
