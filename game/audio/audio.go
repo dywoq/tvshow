aa9d8eb8f107c0b61249ad2a2ebeb459c2247cee
dywoq <dywoq.contact@gmail.com>
2026-09-21 12:14:08 +0300

    game/audio: Implement audio functionality
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- game/audio/audio.go
+++ game/audio/audio.go
@@ -1,16 +1,25 @@
 package audio
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/vorbis"
 )
+
+type Type string
 
 var (
 	mu      sync.Mutex
 	context *audio.Context
-	player  *audio.Player
+	players = map[string]*audio.Player{}
+)
+
+const (
+	TypeOgg Type = "ogg"
 )
 
 func Initializer() error {
@@ -21,4 +30,105 @@ func Initializer() error {
 		return fmt.Errorf("initializing the audio context failed: %v", r)
 	}
 	return nil
+}
+
+// Play converts the file into stream and stores the initialized player
+// into the underlying map. Returns an error if the conversion failed,
+// t is an unknown audio type or the audio player with playerName already
+// exists.
+func Play(t Type, playerName string, filepath string) error {
+	mu.Lock()
+	defer mu.Unlock()
+	if _, ok := players[playerName]; ok {
+		return fmt.Errorf("the audio player %q already exists", playerName)
+	}
+	switch t {
+	case TypeOgg:
+		content, err := os.ReadFile(filepath)
+		if err != nil {
+			return err
+		}
+		stream, err := vorbis.DecodeWithSampleRate(context.SampleRate(), bytes.NewReader(content))
+		if err != nil {
+			return err
+		}
+		p, err := context.NewPlayer(stream)
+		if err != nil {
+			return err
+		}
+		players[playerName] = p
+		return nil
+	}
+	return fmt.Errorf("unknown audio type: %s", string(t))
+}
+
+// Close destroys audio player's resources. Returns an error if it couldn't
+// find a player.
+func Close(playerName string) error {
+	mu.Lock()
+	defer mu.Unlock()
+	p, ok := players[playerName]
+	if !ok {
+		return fmt.Errorf("failed to find the player %q", playerName)
+	}
+	p.PauseAndStopReading()
+	return nil
+}
+
+// IsPlaying checks whether the audio player plays.
+func IsPlaying(playerName string) bool {
+	mu.Lock()
+	defer mu.Unlock()
+	p, ok := players[playerName]
+	if !ok {
+		return false
+	}
+	return p.IsPlaying()
+}
+
+// Rewind rewinds audio of the player to the start.
+func Rewind(playerName string) error {
+	mu.Lock()
+	defer mu.Unlock()
+	p, ok := players[playerName]
+	if !ok {
+		return fmt.Errorf("failed to find the player %q", playerName)
+	}
+	return p.Rewind()
+}
+
+// Pause pauses audio of the player.
+func Pause(playerName string) error {
+	mu.Lock()
+	defer mu.Unlock()
+	p, ok := players[playerName]
+	if !ok {
+		return fmt.Errorf("failed to find the player %q", playerName)
+	}
+	p.Pause()
+	return nil
+}
+
+// SetVolume sets the volume of the audio player.
+func SetVolume(playerName string, volume float64) error {
+	mu.Lock()
+	defer mu.Unlock()
+	p, ok := players[playerName]
+	if !ok {
+		return fmt.Errorf("failed to find the player %q", playerName)
+	}
+	p.SetVolume(volume)
+	return nil
+}
+
+// GetVolume gets the current volume of the audio player. Returns -1
+// if the player wasn't found.
+func GetVolume(playerName string) float64 {
+	mu.Lock()
+	defer mu.Unlock()
+	p, ok := players[playerName]
+	if !ok {
+		return -1
+	}
+	return p.Volume()
 }
--- go.mod
+++ go.mod
@@ -33,7 +33,9 @@ require (
 	github.com/hack-pad/safejs v0.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jeandeaual/go-locale v0.0.0-20250612000132-0ef82f21eade // indirect
+	github.com/jfreymuth/oggvorbis v1.0.5 // indirect
 	github.com/jfreymuth/pulse v0.1.3 // indirect
+	github.com/jfreymuth/vorbis v1.0.2 // indirect
 	github.com/jsummers/gobmp v0.0.0-20230614200233-a9de23ed2e25 // indirect
 	github.com/mattn/go-runewidth v0.0.24 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
